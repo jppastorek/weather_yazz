102d5ed85e14a7ba03b0ed68ae10c2641b7d147e
Jon Pastorek <jppastorek@gmail.com>
2026-08-21 22:27:28 -0500

    add top secret message

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ yazzButton = document.getElementById('yazz-button');
 
 yazzButton.addEventListener('click', () => {
     yazzButton.textContent = 'YAZZ ACTIVATED';
-    console.log('the yazz has been activated');
+    console.log('the yazz has been activated. hail chromelord');
 });
