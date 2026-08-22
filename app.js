f3c2de73d2ca8007ce423e054564701ea5e4e1c5
Jon Pastorek <jppastorek@gmail.com>
2026-08-21 22:18:48 -0500

    Add footer

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ yazzButton = document.getElementById('yazz-button');
 
 yazzButton.addEventListener('click', () => {
     yazzButton.textContent = 'YAZZ ACTIVATED';
+    console.log('the yazz has been activated');
 });
