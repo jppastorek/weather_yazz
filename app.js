b5232a35b85616c2da4626af0cc4e2f6e5342dc3
Jon Pastorek <jppastorek@gmail.com>
2026-08-22 07:02:41 -0500

    Add date and time

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ yazzButton.addEventListener('click', () => {
     yazzButton.textContent = 'YAZZ ACTIVATED';
     console.log('the yazz has been activated. hail chromelord');
 });
+
+function updateClock() {
+    const now = new Date();
+
+    const time = now.toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "2-digit"
+    });
+
+    const date = now.toLocaleDateString("en-US", {
+        weekday: "long",
+        month: "long",
+        day: "numeric"
+    });
+
+    document.querySelector("#current-time").textContent = time;
+    document.querySelector("#current-date").textContent = date.toUpperCase();
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
